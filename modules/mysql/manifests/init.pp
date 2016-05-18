2d5313463b8e236c077cee46ad12da1093fa9c68
Alfredo <alfredo.deluca@gmail.com>
2016-05-18 15:01:16 +1000

    add pass for mysql and script to create a DB

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,5 +1,7 @@
 class mysql {
 
+  $sqlpass="Mysql00"
+
 # Install mysql server
   package { "mysql-server":
     ensure => present,
@@ -9,6 +11,16 @@ class mysql {
   service { "mysql":
     ensure => running,
     require => Package["mysql-server"],
+  }
+  exec { "set-mysql-password":
+    unless => "mysqladmin -uroot -p$sqlpass status",
+    command => "mysqladmin -uroot password $sqlpass",
+    require => Service["mysql"],
+  }
+ # create db called vibrato then a table with id,fname,lname,location
+  exec { "create-db":
+    command => "mysql --host=localhost --user=root --password=$sqlpass < /vagrant/vibrato.sql",
+    require => Exec["set-mysql-password"],
   }
 
 }
