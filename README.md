76c72975ee9c3029d7ee0b1c52db9b48cb98b781
Alfredo <alfredo.deluca@gmail.com>
2016-05-18 17:39:46 +1000

    Final commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Instructions
 0. Install Vagrant and VirtualBox
 1. Clone the repo
-3. vagrant up
+2. vagrant up
+3. Then connect to 192.168.100.10 with a browser
+4. You should see some data 
--- docroot/index.php
+++ docroot/index.php
@@ -1,10 +1,32 @@
 <?php
-
-// Show all information, defaults to INFO_ALL
-phpinfo();
-
-// Show just the module information.
-// phpinfo(8) yields identical results.
-// phpinfo(INFO_MODULES);
-
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = 'Mysql00';
+   $db     = 'vibrato';
+   
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql = 'SELECT id, fname, lname, location FROM employee';
+   mysql_select_db($db);
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   
+   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "EMP ID :{$row['id']}  <br> ".
+         "EMP NAME : {$row['fname']} <br> ".
+         "EMP NAME : {$row['lname']} <br> ".
+         "EMP LOC  : {$row['location']} <br> ".
+         "--------------------------------<br>";
+   }
+   echo "DB: $db\n";   
+   echo "Fetched data successfully\n";
+   
+   mysql_close($conn);
 ?>
