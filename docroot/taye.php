7ffd03b66d80db9791992de31ff04208398a32a4
Alfredo <alfredo.deluca@gmail.com>
2016-06-09 17:23:53 +1000

    final?

--- docroot/taye.php
+++ docroot/taye.php
@@ -1,36 +1,27 @@
 <html>
 <head>
-<style>
-.error {color: #FF0000;}
-</style>
 </head>
 <body>  
 
 <h2>Rellena tu CV</h2>
-<p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Nombre: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
+
+<form method="post">  
+  Nombre: <input type="text" name="nombre" value="">
   <br><br>
-  Apellido: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
+  Apellido: <input type="text" name="apellido" value="">
   <br><br>
-  Contrasena: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
+  Contrasena: <input type="text" name="contrasena" value="">
   <br><br>
-  DNI: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
+  DNI: <input type="text" name="dni" value="">
   <br><br>
   Sexo:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Mujer") echo "checked";?> value="Mujer">Mujer
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Hombre") echo "checked";?> value="Hombre">Hombre
-  <span class="error">* <?php echo $genderErr;?></span>
+  <input type="radio" name="sexo" value="Mujer">Mujer
+  <input type="radio" name="sexo" value="Hombre">Hombre
   <br><br>
   <input type="submit" name="submit" value="Guardar Cambios"> <input type="submit" name="submit" value="Borrar los datos introducidos">  
 </form>
 
 <?php
-echo "<h2>Your Input:</h2>";
 echo $name;
 echo "<br>";
 echo $email;
